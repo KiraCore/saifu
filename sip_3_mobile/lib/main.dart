d8b95de41aee6237b1f8d2646e2f5652b16b60a3
AmanuelYosief <43088213+AmanuelYosief@users.noreply.github.com>
2020-10-11 01:35:42 +0100

    included after_layout, and added navigation to onboarding
    after_layout uses Mixin, this helps to catch the first frame from inistialization of the screen to call a function. This frame checks and calls for either onboarding || login

--- sip_3_mobile/lib/main.dart
+++ sip_3_mobile/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:after_layout/after_layout.dart';
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'screens/onboarding_screen.dart';
@@ -25,16 +26,20 @@ class SplashScreen extends StatefulWidget {
   _SplashScreenState createState() => _SplashScreenState();
 }
 
-class _SplashScreenState extends State<SplashScreen> {
+class _SplashScreenState extends State<SplashScreen> with AfterLayoutMixin<SplashScreen> {
   Future firstTime() async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
     bool _first = (prefs.getBool('first') ?? false);
     if (_first) {
-      Navigator.of(context).push(new MaterialPageRoute(builder: (context) => OnBoarding()));
+      //Navigator.of(context).push(new MaterialPageRoute(builder: (context) => OnBoarding()));
     } else {
       await prefs.setBool('seen', true);
+      Navigator.of(context).pushReplacement(new MaterialPageRoute(builder: (context) => OnBoarding()));
     }
   }
+
+  @override
+  void afterFirstLayout(BuildContext context) => firstTime();
 
   @override
   Widget build(BuildContext context) {
